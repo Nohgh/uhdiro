b7130e67dd472e8109374f0be48f985d125b64a1
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-02-26 15:34:24 +0900

    Feat: header custom [1]

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.scss";
+import Header from "./components/header/Header";
 
 const App = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-      console.log(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-  //440이 넘어가면 중앙에 화면을 440으로 고정
   return (
     <>
       <div className="main">
-        <div>현재 화면의 너비는 {windowWidth}</div>
+        <Header />
       </div>
     </>
   );
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,32 @@
+import "./Header.scss";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__top">
+        <svg
+          className="header__top__bars"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+        </svg>
+        <div className="header__top__logo">Udiro</div>
+      </div>
+      <div className="header__searchbox">
+        <input
+          className="header__searchbox__input"
+          placeholder="건물 이름 및 번호, 강의실 번호 및 이름 검색"
+        ></input>
+        <svg
+          className="header__searchbox__icon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
